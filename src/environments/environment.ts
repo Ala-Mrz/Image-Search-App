e45545b0416cb4da98975434de7dadfab31e756a
Ala-Mrz <ala.marzouk@supinfo.com>
2017-12-14 16:36:55 +0100

    first commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,7 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  PIXABAY_API_KEY: '5825000-7ca7ae076a038d0e91c73d027',
+  PIXABAY_API_URL: 'https://pixabay.com/api/?key=',
 };
